2779774aa64d61192671c4e3fcf584d44888db70
jwyxym <jwyxym@126.com>
2026-08-19 13:54:16 +0800

    将下载链接改为数组

--- site/index.js
+++ site/index.js
@@ -4,5 +4,5 @@ var download_button = document.getElementById('downloadButton');
 fetch('/ver_code.json').then(i => i.json()).then(i => {
     version.innerText = i.versionname
     update_time.innerText = i.update_time
-    download_button.href = i.download_link
+    download_button.href = i.download_link[0]
 });
