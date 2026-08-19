88d23a4555dbd3ce6e5ca2b1117565affa37fbb6
jwyxym <jwyxym@126.com>
2026-08-19 13:49:05 +0800

    修改下载链接

--- site/index.js
+++ site/index.js
@@ -1,6 +1,8 @@
 var version = document.getElementById('version');
 var update_time = document.getElementById('update_time');
+var download_button = document.getElementById('downloadButton');
 fetch('/ver_code.json').then(i => i.json()).then(i => {
-    version.innerText=i.versionname
+    version.innerText = i.versionname
     update_time.innerText = i.update_time
+    download_button.href = i.download_link
 });
