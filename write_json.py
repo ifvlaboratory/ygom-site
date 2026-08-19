2e393c8ac3a20fad9a91df75e4d035caf98d660e
jwyxym <jwyxym@126.com>
2026-08-19 14:31:57 +0800

    新增脚本文件

---
+++ write_json.py
@@ -0,0 +1,24 @@
+import json
+
+config = {
+    "versionname" : "3.13.6",
+    "update_time" : "2026年7月",
+    "download_link" : [
+        "https://files.ygom.top/files/YGOMobile_3.13.6.Apk"
+    ],
+	"pre_release_code" : {}
+}
+
+pre_release_code = """
+100268201	100268201
+"""
+
+for i in pre_release_code.split('\n'):
+    if len(i) == 0:
+        continue
+    code = i.split()
+    config["pre_release_code"][code[0]] = code[1]
+
+path = './site/ver_code.json'
+with open(path, 'w', encoding = 'utf-8') as f :
+	json.dump(config, f, indent = 4, ensure_ascii=False)
